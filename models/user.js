b548ef7153a8faf185bc9557a561316d293f92dc
xtreme6007 <prestonnichols96@gmail.com>
2020-10-05 20:02:49 -0500

    added in jawsDb credintials

--- models/user.js
+++ models/user.js
@@ -16,7 +16,13 @@ module.exports = function(sequelize, DataTypes) {
     password: {
       type: DataTypes.STRING,
       allowNull: false
+    },
+
+    car: {
+      type:DataTypes.STRING
+
     }
+
   });
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
   User.prototype.validPassword = function(password) {
