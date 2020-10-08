85d9a9053d36806ecb40cc67fba33ee7813babc6
xtreme6007 <prestonnichols96@gmail.com>
2020-10-07 21:36:33 -0500

    fixed handlebars problems in js, and general updates

--- public/js/script.js
+++ public/js/script.js
@@ -44,8 +44,7 @@ $(document).ready(() => {
 
       // Getting references to our form and input
   const signUpForm = $("form.signup");
-  const emailInput = $("input#email-input");
-  const passwordInput = $("input#password-input");
+  
 
   // When the signup button is clicked, we validate the email and password are not blank
   signUpForm.on("submit", event => {
@@ -53,28 +52,28 @@ $(document).ready(() => {
     const userData = {
       email: emailInput.val().trim(),
       password: passwordInput.val().trim(),
-      car: carInput.val().trim(),
-      name: nameInput.val.trim()
+      //car: carInput.val().trim(),
+      //name: nameInput.val.trim()
       
     };
 
-    if (!userData.email || !userData.password || !userData.car || !userData.name) {
+    if (!userData.email || !userData.password /*|| !userData.car || !userData.name*/) {
       return;
     }
     // If we have an email and password, run the signUpUser function
-    signUpUser(userData.email, userData.password, userData.car, userData.name);
+    signUpUser(userData.email, userData.password, /*userData.car, userData.name*/);
     emailInput.val("");
     passwordInput.val("");
   });
 
   // Does a post to the signup route. If successful, we are redirected to the members page
   // Otherwise we log any errors
-  function signUpUser(email, password, car, name) {
+  function signUpUser(email, password/*, car, name*/) {
     $.post("/api/signup", {
       email: email,
       password: password,
-      car: car,
-      name: name
+      /*car: car,
+      name: name*/
     })
       .then(() => {
         window.location.replace("/members");
