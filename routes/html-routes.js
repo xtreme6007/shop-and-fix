3bf5314abaa941d21cea16b5afd2cb7207fb6ca4
xtreme6007 <prestonnichols96@gmail.com>
2020-10-10 13:10:19 -0500

    added in if statement for switching for employee or customer

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -24,7 +24,13 @@ module.exports = function(app) {
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, (req, res) => {
-    res.render("customerDash");
+    
+    if (req.user.role === "customer") {
+      res.render("customerDash");
+    } else if (req.user.role === "employee") {
+    res.render("employeeDash")
+    }
+
   });
   app.get("/services", isAuthenticated, (req, res) => {
     res.render("services");
