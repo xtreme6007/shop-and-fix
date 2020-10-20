0d218ea33fb2cbed566a519cc3dc8d0cafb861e9
xtreme6007 <prestonnichols96@gmail.com>
2020-10-20 11:38:17 -0500

    added changes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -9,8 +9,9 @@ module.exports = function(app) {
     // If the user already has an account send them to the members page
     if (req.user) {
       res.redirect("/members");
-    }
+    } else {
     res.render("index")
+    }
   });
 
   app.get("/login", (req, res) => {
